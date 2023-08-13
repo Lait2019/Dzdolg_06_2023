e1af9fc3851d2467d7dfdcb2a4706697815e9c77
Lait <mikhail.ryzhov.99@mail.ru>
2023-08-13 15:56:52 +0300

    Dz68

---
+++ alldz06_2023/Dz68/Program.cs
@@ -0,0 +1,22 @@
+﻿int M = 3, N = 2;
+int A(int m, int n)
+{
+    int v;
+    if (m == 0) 
+    {
+        v = n+1;
+        return v;
+    }
+    else if ((m > 0) && (n == 0)) 
+    {
+        v = A((m-1), 1);
+        return v;
+    }
+    else if ((m > 0) && (n > 0)) 
+    {
+        v = A(m-1, A(m, n-1));
+        return v;
+    }
+    else return 0;
+}
+Console.Write(A(M, N));
